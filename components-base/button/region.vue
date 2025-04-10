ab2bcbbb5a34c8189750fe63c3929cd62dc6b72e
Elkina Varvara <elkinavarvara1999@gmail.com>
2025-04-10 11:18:53 +0300

    add footer with adaptive

---
+++ components-base/button/region.vue
@@ -0,0 +1,59 @@
+<template>
+  <button
+    class="btn-region"
+    :class="'btn-region--' + name"
+  >
+    {{ region }}
+  </button>
+</template>
+
+<script setup>
+defineProps({
+  region: {
+    type: String,
+    default: 'г. Нижний Новгород',
+  },
+  name: {
+    type: String,
+    default: 'dark',
+    validator: value => ['light', 'dark'].includes(value),
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.btn-region {
+  height: fit-content;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  font: 400 16px/1 var(--font);
+  transition: color var(--time);
+  color: var(--text-tertiary);
+  &::before {
+    content: "";
+    display: block;
+    width: 20px;
+    height: 20px;
+    mask-repeat: no-repeat;
+    mask-position: center;
+    mask-size: contain;
+    mask-image: url(/icons/pin.svg);
+    background-color: var(--icon-accent);
+  }
+  &--light {
+    color: var(--text-secondary);
+    &::before{
+        background-color: var(--icon-accent-2);
+    }
+  }
+}
+@include hover {
+  .btn-region:hover {
+    color: var(--text-accent);
+  }
+  .btn-region--light:hover {
+    color: var(--icon-accent-2);
+  }
+}
+</style>
--- components/app/footer.vue
+++ components/app/footer.vue
@@ -29,10 +29,10 @@
             </div>
             <ul class="footer__nav-list">
               <li class="footer__nav-item">
-                <NuxtLink class="footer__nav-link">Переезды</NuxtLink>
+                <NuxtLink to="#" class="footer__nav-link">Переезды</NuxtLink>
               </li>
               <li class="footer__nav-item">
-                <NuxtLink class="footer__nav-link">Акции</NuxtLink>
+                <NuxtLink to="#" class="footer__nav-link footer__nav-link--accent">Сервисный центр</NuxtLink>
               </li>
             </ul>
           </div>
@@ -42,15 +42,15 @@
             </div>
             <ul class="footer__nav-list">
               <li class="footer__nav-item">
-                <NuxtLink class="footer__nav-link">Доставка для ритейлеров</NuxtLink>
+                <NuxtLink to="#" class="footer__nav-link">Доставка для ритейлеров</NuxtLink>
               </li>
               <li class="footer__nav-item">
-                <NuxtLink class="footer__nav-link">Доставка до складов маркетплейсов</NuxtLink>
+                <NuxtLink to="#" class="footer__nav-link">Доставка до складов маркетплейсов</NuxtLink>
               </li>
             </ul>
           </div>
         </nav>
-        <UiSwitcherRegion/>
+        <ButtonRegion region="г. Москва" name="light" class="footer__region" />
       </div>
       <div class="footer__bottom">
         <a
@@ -102,7 +102,13 @@
     padding-top: 84px;
     padding-bottom: 48px;
     display: flex;
+    flex-wrap: wrap;
     gap: 30px;
+    justify-content: space-between;
+  }
+
+  &__left{
+    width: 23%;
   }
 
   &__logo-link {
@@ -113,9 +119,10 @@
   }
 
   &__logo-img {
-    width: 100%;
+    width: 119px;
     height: 100%;
     object-fit: contain;
+    margin-bottom: 8px;
   }
 
   &__logo-text{
@@ -129,12 +136,16 @@
 
   &__descr{
     font-size: 14px;
+    line-height: 1.25;
     width: 100%;
+    opacity: 0.5;
+    min-width: 184px;
   }
 
   &__nav{
     display: flex;
-    gap: 64px;
+    flex-wrap: wrap;
+    gap: 48px 64px;
   }
 
   &__nav-list{
@@ -147,6 +158,23 @@
     font-size: 16px;
     line-height: 1.1;
     letter-spacing: 0.01;
+    transition: color var(--time);
+  }
+
+  &__nav-link--accent{
+    color: var(--text-accent-2);
+    display: flex;
+    &::after{
+        content: '';
+        display: block;
+        width: 20px;
+        height: 20px;
+        mask-repeat: no-repeat;
+        mask-position: center;
+        mask-size: contain;
+        mask-image: url(/icons/arrow-transition.svg);
+        background-color: var(--icon-accent-2);
+    }
   }
 
   &__subtitle{
@@ -155,20 +183,21 @@
   }
 
   &__bottom {
+    font-size: 14px;
     color: var(--text-secondary-t50);
     display: flex;
     justify-content: space-between;
     align-items: center;
     padding: 48px 0;
     gap: 15px;
-    border-top: 1px solid var(--stroke-secondary);
+    border-top: 1px solid var(--stroke-secondary-t10);
   }
 
   &__design,
   &__copyright,
   &__policy,
   &__webshop {
-    font-size: 16px;
+    font-size: inherit;
     transition: color var(--time);
   }
 
@@ -185,6 +214,10 @@
   }
 }
 @include hover {
+    .footer__nav-link:hover{
+        color: var(--text-accent-2)
+    }
+
     .footer__design:hover,
     .footer__policy:hover,
     .footer__webshop:hover{
@@ -194,6 +227,20 @@
 @include desktop {
 }
 @include tablet-s {
+    .footer{
+        &__region{
+            display: none;
+        }
+        &__bottom{
+            flex-direction: column;
+            align-items: start;
+            gap: 24px;
+        }
+        &__development{
+            align-items: start;
+            gap: 24px;
+        }
+    }
 }
 @include mobile {
 }
